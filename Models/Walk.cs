73129257ed2b089fd38620619fbb80b652adb5a1
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-07-12 20:48:17 -0500

    Added display names for DogId and WalkerId, display formatting for Date, and changed Date to DateTime format

--- Models/Walk.cs
+++ Models/Walk.cs
@@ -1,13 +1,25 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace DogGo.Models
 {
     public class Walk
     {
         public int Id { get; set; }
+        
+        [DisplayName("Dog")]
         public int DogId { get; set; }
+        
         public Dog Dog { get; set; }
+        
+        [DisplayName("Walker")]
         public int WalkerId {get; set; }
+        
         public Walker Walker { get; set;}
+        
         public int Duration { get; set; }
-        public DateOnly Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Date { get; set; }
     }
 }
